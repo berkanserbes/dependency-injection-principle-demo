ea1f139c3bcdc42aa7f950864f99d97983ba5cf1
Berkan Serbes <berkan_serbes@hotmail.com>
2025-05-13 00:20:00 +0300

    Create sample projects

---
+++ DependencyInjectionDemo/DependencyInjectionDemo.ExampleConsoleAppWithoutDI/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace DependencyInjectionDemo.ExampleConsoleAppWithoutDI;
+
+public class Program
+{
+	static void Main(string[] args)
+	{
+		Console.WriteLine("Hello, World!");
+	}
+}
---
+++ DependencyInjectionDemo/DependencyInjectionDemp.ExampleConsoleAppWithDI/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace DependencyInjectionDemp.ExampleConsoleAppWithDI;
+
+public class Program
+{
+	static void Main(string[] args)
+	{
+		Console.WriteLine("Hello, World!");
+	}
+}
